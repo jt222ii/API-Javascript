7cbd6c1c3961b42d9a1b61924a141c7e7822f40e
Jonas Tornfors <jt222ii@student.lnu.se>
2015-12-04 16:37:53 +0000

    map and labels now show

--- main.js
+++ main.js
@@ -1,23 +1,7 @@
 "use strict";
 var mailMap = {
     main: function(){
-        //checkAuth();
-        mailMap.map.initMap();
+        gmaps.initMap();
+        mail.checkAuth();
     }
 };
-
-mailMap.map = {
-    map: {},
-    initMap: function() {
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-      });
-    },
-    asd: function() {
-        alert("test");
-    }
-}
-
-
-//window.onload(mailMap.main());
--- maps.js
+++ maps.js
@@ -1,8 +1,9 @@
-
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
+var gmaps = {
+  map:{},
+  initMap:function() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
 }
